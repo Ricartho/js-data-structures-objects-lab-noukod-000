6f613d586a54ff4b28308cf77081c34b70addd27
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-10 14:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+const driver = {};
+
+
+function updateDriverWithKeyAndValue(obj, key, value){
+  const newDriver = {...obj};
+  newDriver[key] = value;
+  return newDriver;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
